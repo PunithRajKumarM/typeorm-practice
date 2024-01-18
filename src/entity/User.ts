612559d8c5ba3fe4e95ce3b9a5e5cae44fa2a927
PunithRajKumarM <punithrajkumar.m@celestialsys.com>
2024-01-18 17:18:13 +0530

    for todo, commented the one to one relation

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -25,12 +25,12 @@ export class User {
 
   //cascade : true, means operations such as saving, updating, or removing the owning entity will also be applied to the related Profile entity.
   //eager : true means it will allows the entity to display all data along with the relation data
-  @OneToOne(() => Profile, (profile) => profile.user, {
-    cascade: true,
-    eager: true,
-  })
-  @JoinColumn()
-  profile: Profile;
+  // @OneToOne(() => Profile, (profile) => profile.user, {
+  //   cascade: true,
+  //   eager: true,
+  // })
+  // @JoinColumn()
+  // profile: Profile;
 
   @OneToMany(() => Todo, (todo) => todo.user)
   todos: Todo[];
